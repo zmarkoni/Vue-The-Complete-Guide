f27888d8cf4ddae231a393afea5435c6d6ed8a7b
Zoran Markovic <zoran.markovic@netcentric.biz>
2020-12-30 12:14:41 +0100

    vue Computed

--- basics-05-using-the-native-event-object/app.js
+++ basics-05-using-the-native-event-object/app.js
@@ -5,7 +5,18 @@ const app = Vue.createApp({
 			name: '',
 		};
 	},
+	computed: {
+		// it's also methods but we call them as variables, without parenthesis
+		// <p>Your Name: {{ fullName }}</p>
+		fullName() {
+			if (this.name === '') {
+				return '';
+			}
+			return this.name + ' ' + 'Markovic';
+		},
+	},
 	methods: {
+    // events need to be METHODS
 		setName(event, lastName) {
 			this.name = event.target.value;
 		},
@@ -16,9 +27,9 @@ const app = Vue.createApp({
 			this.counter = this.counter - num;
 			// this.counter--;
 		},
-	  resetInput() {
-      this.name = '';
-    }
+		resetInput() {
+			this.name = '';
+		},
 	},
 });
 
