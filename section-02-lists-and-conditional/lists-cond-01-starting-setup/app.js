dda2cfb3c8303c88cb8c4fc0ca4e6a6f391c6ef2
Zoran Markovic <zoran.markovic@netcentric.biz>
2021-01-01 18:58:47 +0100

    Lists done

--- section-02-lists-and-conditional/lists-cond-01-starting-setup/app.js
+++ section-02-lists-and-conditional/lists-cond-01-starting-setup/app.js
@@ -1,7 +1,22 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return {
+		enteredGoalValue: '',
+		goals: [],
+		userData: {
+			name: 'Zoki',
+			age: 3,
+		},
+	};
   },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredGoalValue);
+    },
+    removeGoal(index) {
+      this.goals.splice(index, 1);
+    }
+  }
 });
 
 app.mount('#user-goals');
