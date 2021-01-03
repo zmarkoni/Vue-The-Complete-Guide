f3f8ce2322c827bf71b89edd17864cdc953a4140
Zoran Markovic <zoran.markovic@netcentric.biz>
2021-01-03 12:03:56 +0100

    Lists assignment done

--- section-02-lists-and-conditional/lists-cond-assignment-solution/app.js
+++ section-02-lists-and-conditional/lists-cond-assignment-solution/app.js
@@ -1,24 +1,22 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      enteredValue: '',
-      tasks: [],
-      taskListIsVisible: true,
-    };
-  },
-  computed: {
-    buttonCaption() {
-      return this.taskListIsVisible ? 'Hide List' : 'Show List';
-    },
-  },
-  methods: {
-    addTask() {
-      this.tasks.push(this.enteredValue);
-    },
-    toggleTaskList() {
-      this.taskListIsVisible = !this.taskListIsVisible;
-    },
-  },
-});
-
-app.mount('#assignment');
+	data() {
+		return {
+			enteredText: '',
+			tasks: [],
+			listIsVisible: true,
+		};
+	},
+	computed: {
+		buttonCaption() {
+			return this.listIsVisible ? 'Hide List' : 'Show List';
+		},
+	},
+	methods: {
+		addTask() {
+			this.tasks.push(this.enteredText);
+		},
+		toggleVisibility() {
+			this.listIsVisible = !this.listIsVisible;
+		},
+	},
+}).mount('#assignment');
