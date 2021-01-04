9b238353c2fcd1415e451b9a77dfdc0a0f165645
Zoran Markovic <zoran.markovic@netcentric.biz>
2021-01-04 12:21:18 +0100

    Vue behind the scenes

---
+++ section-5-vue-behind-the-scenes/behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,75 @@
+const app = Vue.createApp({
+	data() {
+		return {
+			currentUserInput: '',
+			message: 'Vue is great!',
+		};
+	},
+	methods: {
+		saveInput(event) {
+			this.currentUserInput = event.target.value;
+		},
+		setText() {
+			// this.message = this.currentUserInput;
+			this.message = this.$refs.userText.value;
+			//console.log(this.message = this.$refs.userText); // HTML Object
+		},
+	},
+	beforeCreate() {
+		console.log('beforeCreate()');
+		// send HTTP, set timers...
+	},
+	created() {
+		console.log('created()');
+	},
+	beforeMount() {
+		console.log('beforeMount()');
+	},
+	mounted() {
+		console.log('mounted()');
+	},
+	mounted() {
+		console.log('mounted()');
+	},
+	beforeUpdate() {
+		console.log('beforeUpdate()');
+	},
+	updated() {
+		console.log('updated()');
+	},
+	beforeUnmount() {
+		console.log('beforeUnmount()');
+	},
+	unmounted() {
+		console.log('unmounted()');
+	}
+});
+
+app.mount('#app');
+
+setTimeout(function () {
+  app.unmount();
+}, 3000);
+
+// Vue works like a native JS PROXY
+// ....
+
+const data = {
+  message: 'Hello!',
+  longMessage: 'Hello! World!'
+};
+
+const handler = {
+  set(target, key, value) {
+    if (key === 'message') {
+      target.longMessage = value + ' World!';
+    }
+    target.message = value;
+  }
+};
+
+const proxy = new Proxy(data, handler);
+
+proxy.message = 'Hello!!!!';
+
+//console.log(proxy.longMessage);
