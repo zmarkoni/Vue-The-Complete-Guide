aee9d2f5d7ede373cd89f4f4c8d8c93f058a2bb0
Zoran Markovic <zoran.markovic@netcentric.biz>
2021-01-07 20:41:54 +0100

    Vue components

--- section-08-component-comunication/cmp-communication-01-starting-setup/src/App.vue
+++ section-08-component-comunication/cmp-communication-01-starting-setup/src/App.vue
@@ -3,9 +3,19 @@
     <header>
       <h1>My Friends</h1>
     </header>
+    <new-friend @add-contact="addContact"></new-friend>
     <ul>
-      <friend-contact></friend-contact>
-      <friend-contact></friend-contact>
+      <friend-contact
+        v-for="friend in friends"
+        :key="friend.id"
+        :id="friend.id"
+        :name="friend.name"
+        :phone-number="friend.phone"
+        :email-address="friend.email"
+        :is-favorite="friend.isFavorite"
+        @toggle-favorite="toggleFavoriteStatus"
+        @delete="deleteContact"
+      ></friend-contact>
     </ul>
   </section>
 </template>
@@ -30,6 +40,27 @@ export default {
       ],
     };
   },
+  methods: {
+    toggleFavoriteStatus(friendId) {
+      const identifiedFriend = this.friends.find(
+        (friend) => friend.id === friendId
+      );
+      identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
+    },
+    addContact(name, phone, email) {
+      const newFriendContact = {
+        id: new Date().toISOString(),
+        name: name,
+        phone: phone,
+        email: email,
+        isFavorite: false
+      };
+      this.friends.push(newFriendContact);
+    },
+    deleteContact(friendId) {
+      this.friends = this.friends.filter(friend => friend.id !== friendId);
+    }
+  }
 };
 </script>
 
@@ -54,12 +85,14 @@ header {
   width: 90%;
   max-width: 40rem;
 }
-#app ul {
+#app ul,
+#app form {
   margin: 0;
   padding: 0;
   list-style: none;
 }
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -88,5 +121,19 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
 }
 </style>
---
+++ section-08-component-comunication/cmp-communication-01-starting-setup/src/components/NewFriend.vue
@@ -0,0 +1,45 @@
+<template>
+	<form @submit.prevent="sumbitData">
+		<div>
+			<label>Name</label>
+			<input type="text" v-model="enteredName" />
+		</div>
+		<div>
+			<label>Phone</label>
+			<input type="tel" v-model="enteredPhone" />
+		</div>
+		<div>
+			<label>E-mail</label>
+			<input type="email" v-model="enteredEmail" />
+		</div>
+		<div>
+			<button>Add Contact</button>
+		</div>
+	</form>
+</template>
+
+<script>
+export default {
+	emits: ['add-contact'],
+	data() {
+		return {
+			enteredName: '',
+			enteredPhone: '',
+			enteredEmail: '',
+		};
+	},
+	methods: {
+		sumbitData() {
+			// emit custom event from this component,
+			// and forward data to any component which will subscribe/listen to it
+			// in our case in <new-friend @add-contact="addContact"></new-friend> in App.vue
+			this.$emit(
+				'add-contact',
+				this.enteredName,
+				this.enteredPhone,
+				this.enteredEmail
+			);
+		},
+	},
+};
+</script>
