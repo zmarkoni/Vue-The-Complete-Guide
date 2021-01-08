fb1392e6d2d291b06d70d30ebf4bbeaa63d7e8c0
Zoran Markovic <zoran.markovic@netcentric.biz>
2021-01-08 22:36:52 +0100

    Section 9 Components deed dive finished

--- section-09-components-deep-dive/cmp-adv-07-keeping-dynamic-components-alive/src/App.vue
+++ section-09-components-deep-dive/cmp-adv-07-keeping-dynamic-components-alive/src/App.vue
@@ -1,55 +1,59 @@
 <template>
-  <div>
-    <the-header></the-header>
-    <!-- <TheHeader /> -->
-    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
-    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
-    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+	<div>
+		<the-header></the-header>
+		<!-- <TheHeader /> -->
+		<button @click="setSelectedComponent('active-goals')">
+			Active Goals
+		</button>
+		<button @click="setSelectedComponent('manage-goals')">
+			Manage Goals
+		</button>
+		<!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
     <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals>-->
-    <keep-alive>
-      <component :is="selectedComponent"></component>
-    </keep-alive>
-  </div>
+		<keep-alive>
+			<component :is="selectedComponent"></component>
+		</keep-alive>
+    <!-- Here, <component> will swap with what ever component is returned to :is
+      In our case what ever 'selectedComponent' returns, which is 'active-goals' or 'manage-goals'
+      -->
+	</div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
-// import BadgeList from './components/BadgeList.vue';
-// import UserInfo from './components/UserInfo.vue';
-// import CourseGoals from './components/CourseGoals.vue';
 import ActiveGoals from './components/ActiveGoals.vue';
 import ManageGoals from './components/ManageGoals.vue';
 
 export default {
-  components: {
-    TheHeader,
-    ActiveGoals,
-    ManageGoals,
-  },
-  data() {
-    return {
-      selectedComponent: 'active-goals',
-      activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
-      },
-    };
-  },
-  methods: {
-    setSelectedComponent(cmp) {
-      this.selectedComponent = cmp;
-    },
-  },
+	components: {
+		TheHeader,
+		ActiveGoals,
+		ManageGoals,
+	},
+	data() {
+		return {
+			selectedComponent: 'active-goals',
+			activeUser: {
+				name: 'Maximilian Schwarzmüller',
+				description: 'Site owner and admin',
+				role: 'admin',
+			},
+		};
+	},
+	methods: {
+		setSelectedComponent(cmp) {
+			this.selectedComponent = cmp;
+		},
+	},
 };
 </script>
 
 <style>
 html {
-  font-family: sans-serif;
+	font-family: sans-serif;
 }
 
 body {
-  margin: 0;
+	margin: 0;
 }
 </style>
--- section-09-components-deep-dive/cmp-adv-07-keeping-dynamic-components-alive/src/components/ManageGoals.vue
+++ section-09-components-deep-dive/cmp-adv-07-keeping-dynamic-components-alive/src/components/ManageGoals.vue
@@ -1,6 +1,38 @@
 <template>
-  <div>
-    <h2>Manage Goals</h2>
-    <input type="text" />
-  </div>
+	<div>
+		<h2>Manage Goals</h2>
+		<input type="text" ref="goal" />
+		<button @click="setGoal">Set Goal</button>
+		<teleport to="body">
+			<error-alert v-if="inputIsInvalid">
+				<h2>Input is invalid!</h2>
+				<p>Please add a few characters...</p>
+				<button @click="errorConfirm">Confirm</button>
+			</error-alert>
+		</teleport>
+	</div>
 </template>
+
+<script>
+import ErrorAlert from './ErrorAlert.vue';
+
+export default {
+	components: { ErrorAlert },
+	data() {
+		return {
+			inputIsInvalid: false,
+		};
+	},
+	methods: {
+		setGoal() {
+			const enteredValue = this.$refs.goal.value;
+			if (enteredValue === '') {
+				this.inputIsInvalid = true;
+			}
+		},
+		errorConfirm() {
+			this.inputIsInvalid = false;
+		},
+	},
+};
+</script>
