56b85cfb85a53b42ae775c1dfaf459dd2335b454
Zoran Markovic <zoran.markovic@netcentric.biz>
2021-01-22 11:30:44 +0100

    Section 15 Vuex create Store and manage mutations

--- section-15-vuex-store/src/App.vue
+++ section-15-vuex-store/src/App.vue
@@ -1,14 +1,32 @@
 <template>
-  <base-container title="Vuex"></base-container>
+  <base-container title="Vuex">
+    <the-counter></the-counter>
+    <button @click="addOne">Add 10</button>
+    <change-counter></change-counter>
+  </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
+import TheCounter from './components/TheCounter.vue';
 
 export default {
   components: {
     BaseContainer,
+    TheCounter,
+    ChangeCounter,
   },
+  methods: {
+    addOne() {
+      //this.$store.commit('increase', {value: 10}); 
+      // or
+      this.$store.commit({
+        type: 'increase',
+        value: 10
+      });
+    }
+  }
 };
 </script>
 
---
+++ section-15-vuex-store/src/components/ChangeCounter.vue
@@ -0,0 +1,14 @@
+<template>
+	<button @click="addOne">Add 2</button>
+</template>
+
+<script>
+export default {
+	methods: {
+		addOne() {
+            // trigger mutation 'incerement which is defined in main.js'
+			this.$store.commit('increment');
+		},
+	},
+};
+</script>
