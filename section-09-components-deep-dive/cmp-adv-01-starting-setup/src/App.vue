ddf7ec36672397808a018fa514a42536d4c8a1f9
Zoran Markovic <zoran.markovic@netcentric.biz>
2021-05-24 23:29:57 +0200

    update section 9 Slots

--- section-09-components-deep-dive/cmp-adv-01-starting-setup/src/App.vue
+++ section-09-components-deep-dive/cmp-adv-01-starting-setup/src/App.vue
@@ -10,7 +10,7 @@
 		></user-info>
 		<course-goals #default="slotProps">
 			<h2>{{ slotProps.item }}</h2>
-			<p>{{ slotProps['another-prop'] }}</p>
+			<p>{{ slotProps['another-prop'] }} nece da radi nesto</p>
 		</course-goals>
 	</div>
 </template>
